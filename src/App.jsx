65fad9b6a26eb5fc01af745f719faf4b964da364
victoriaQuintero <victoria170460@gmail.com>
2023-02-10 20:19:46 -0400

    Creacion de proyecto

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import Nav from './components/Nav'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Nav></Nav>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Nav.jsx
@@ -0,0 +1,9 @@
+function Nav(){
+    return(
+        <div className="Nav">
+            <h1>holi</h1>
+        </div>
+    );
+}
+
+export default Nav;
